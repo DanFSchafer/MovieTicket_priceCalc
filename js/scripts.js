7051c6393e701f0b596e9ecf6d116e13034f68d6
Ashley Riehl and Dan Schafer <capriciash@gmail.com>
2016-07-06 19:46:23 -0700

    add Ticket object and cost prototype. logic to calculate ticket price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,38 @@
+function Ticket(age, name, time, cost) {
+  this.age = age;
+  this.name = name;
+  this.time = time;
+}
+Ticket.prototype.cost = function() {
+  currentCost = 0
+  if (this.time === "matinee") {
+    currentCost = 4;
+  } else {
+      currentCost = 6;
+  }
+  if (this.age === "youth") {
+    currentCost += 1;
+  } else if (this.age === "adult") {
+    currentCost += 3;
+  } if ((this.name === "movie2") || (this.name === "movie3")) {
+    currentCost +=2;
+  }
+  return currentCost;
 
+}
 
 $(function() {
   $("#options form").submit(function(event) {
   event.preventDefault();
-  var movie = $("input:radio[name=movie]:checked").val();
+  var name = $("input:radio[name=movie]:checked").val();
   var age = $("input:radio[name=age]:checked").val();
   var time = $("input:radio[name=time]:checked").val();
-  console.log(movie, age, time);
+  console.log(name, age, time);
 
+  var newTicket = new Ticket(age, name, time);
+  console.log (newTicket);
 
-  });
+  newTicket.cost();
+  console.log(newTicket.cost());
+});
 });
